67744fbb825c9aa17af5be5edbe7a4d5e663d62d
Claudio Malvino <claudiormal@gmail.com>
2026-04-07 15:03:05 +0100

    Changed the operator[] to size_t implementation for indexing with no bounds checking, this is will be the performant implementation for access. Added an at() accessor which will include built-in bounds checking and allowes for negative valued indexes accesseing. Included a default constructor which sets the data = nullptr and the size = 0.

--- include/tensile/dense_vector.hpp
+++ include/tensile/dense_vector.hpp
@@ -8,6 +8,10 @@ class DenseVector : private MemStorage<T, Alignment>
 {
 
 public:
+
+    // Default constructor
+    explicit DenseVector(): MemStorage<T, Alignment>() {}
+
     // Size allocation constructor
     explicit DenseVector(const size_t size): MemStorage<T, Alignment>(size) {}
 
@@ -16,26 +20,38 @@ public:
         std::uninitialized_copy(list.begin(), list.end(), this->data());
     }
 
+
     using MemStorage<T,Alignment>::size;
     using MemStorage<T,Alignment>::data;
 
     // Element accessor/assigner
-    [[nodiscard]] auto operator[](int index) -> T& {
-        assert(index < this->size());
-        if (index < 0) {
+    [[nodiscard]] auto operator[](size_t index) -> T& {
+        return this->data()[index];
+    }
+
+    // Const element accessor/assigner
+    [[nodiscard]] auto operator[](size_t index) const -> const T& {
+        return this->data()[index];
+    }
+
+    // Const accesses to elements with bound checking and allows for negative indexing
+    [[nodiscard]] auto at(const int index) const -> const T& {
+        assert(index < static_cast<int>(this->size()));
+        if (index < 0 && index + static_cast<int>(this->size()) >= 0) {
             return this->data()[this->size() + index];
         }
         return this->data()[index];
     }
 
-    // Const element accessor/assigner
-    [[nodiscard]] auto operator[](int index) const -> const T& {
-        assert(index < this->size());
-        if (index < 0) {
+    // Const accesses to elements with bound checking and allows for negative indexing
+    [[nodiscard]] auto at(const int index) -> T& {
+        assert(index < static_cast<int>(this->size()));
+        if (index < 0 && index + static_cast<int>(this->size()) >= 0) {
             return this->data()[this->size() + index];
         }
         return this->data()[index];
     }
+
     // Self scalar additions
     void operator+=(const T scalar) {
         for (size_t i = 0; i < this->size(); ++i) {
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,45 +1,54 @@
 #include <gtest/gtest.h>
 #include <tensile.hpp>
 
-TEST(DenseVectorTest, VectorInitializationWorks) {
+class DenseVectorTest : public testing::Test {
+protected:
+
     DenseVector<double> vec{1.0, 2.0, 3.0, 4.0, 5.0};
+    DenseVector<double> empty_vec;
+};
+
+TEST_F(DenseVectorTest, VectorInitializationWorks) {
     ASSERT_EQ(vec.size(), 5) << "vec.size() does not equal 5, instead is " << vec.size() << '\n' ;
+    for (size_t i = 0; i < vec.size(); ++i) {
+        ASSERT_EQ(vec[i], static_cast<double>(i + 1));
+    }
 }
 
-TEST(DenseVectorTest, ElementRetrievalandAssignmentWorks) {
-    DenseVector<double> vec{1.0, 2.0, 3.0, 4.0, 5.0};
-    ASSERT_EQ(vec[0], 1) << "vec[0] does not equal 1, instead is " << vec[0] << '\n';
+TEST_F(DenseVectorTest, ElementRetrievalandAssignmentNoBoundsChecking) {
+    ASSERT_EQ(vec[0], 1.0) << "vec[0] does not equal 1, instead is " << vec[0] << '\n';
     vec[1] = 1.5;
     ASSERT_EQ(vec[1], 1.5) << "vec[1] does not equal 1,5, instead is " << vec[1] << '\n';
-    ASSERT_EQ(vec[-1], 5.0) << "vec[-1] does not equal 5.0, instead is " << vec[-1] << '\n';
 }
 
-TEST(DenseVectorTest, SelfAdditionWorks) {
-    DenseVector<double> vec{1.0, 2.0, 3.0, 4.0, 5.0};
+TEST_F(DenseVectorTest, ElementRetrievalandAssignmentBoundsChecking) {
+    ASSERT_EQ(vec.at(0), 1.0);
+    vec.at(1) = 1.5;
+    ASSERT_EQ(vec.at(1), 1.5);
+    ASSERT_EQ(vec.at(-1), 5.0);
+}
+TEST_F(DenseVectorTest, SelfAdditionWorks) {
     vec += 1.0;
     for (size_t i = 0; i < vec.size(); ++i) {
         ASSERT_EQ(vec[i], i + 2.0);
     }
 }
 
-TEST(DenseVectorTest, SelfSubtractionWorks) {
-    DenseVector<double> vec{1.0, 2.0, 3.0, 4.0, 5.0};
+TEST_F(DenseVectorTest, SelfSubtractionWorks) {
     vec -= 1.0;
     for (size_t i = 0; i < vec.size(); ++i) {
         ASSERT_EQ(vec[i], static_cast<double>(i));
     }
 }
 
-TEST(DenseVectorTest, SelfMultiplicationWorks) {
-    DenseVector<double> vec{1.0, 2.0, 3.0, 4.0, 5.0};
+TEST_F(DenseVectorTest, SelfMultiplicationWorks) {
     vec *= 1.0;
     for (size_t i = 0; i < vec.size(); ++i) {
         ASSERT_EQ(vec[i], static_cast<double>(i + 1));
     }
 }
 
-TEST(DenseVectorTest, SelfDivisionWorks) {
-    DenseVector<double> vec{1.0, 2.0, 3.0, 4.0, 5.0};
+TEST_F(DenseVectorTest, SelfDivisionWorks) {
     vec /= 2.0;
     for (size_t i = 0; i < vec.size(); ++i) {
         ASSERT_EQ(vec[i], static_cast<double>((i + 1.0) / 2.0));
