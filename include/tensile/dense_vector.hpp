9e7aa455bc375e8b22c1d7a464d32e583dd15ea2
Claudio Malvino <claudiormal@gmail.com>
2026-04-16 11:14:21 +0100

    Implemented operators for STL containers, such as std::vector and std::array with the use of std::span<const T>. Also implemented arithmetic operators for DenseVector - DenseVector operations.

--- include/tensile/dense_vector.hpp
+++ include/tensile/dense_vector.hpp
@@ -1,6 +1,8 @@
 // This file will describe the 1D vector class and implementation
 
 #pragma once
+#include <cmath>
+#include <ostream>
 #include "core/memory_alloc.hpp"
 
 template<typename T, std::size_t Alignment = 64>
@@ -8,6 +10,9 @@ class DenseVector : private MemStorage<T, Alignment>
 {
 
 public:
+
+    using MemStorage<T,Alignment>::size;
+    using MemStorage<T,Alignment>::data;
 
     // Default constructor
     explicit DenseVector(): MemStorage<T, Alignment>() {}
@@ -20,25 +25,25 @@ public:
         std::uninitialized_copy(list.begin(), list.end(), this->data());
     }
 
-
-    using MemStorage<T,Alignment>::size;
-    using MemStorage<T,Alignment>::data;
-
     // Element accessor/assigner
     [[nodiscard]] auto operator[](size_t index) -> T& {
         return this->data()[index];
     }
 
     // Const element accessor/assigner
-    [[nodiscard]] auto operator[](size_t index) const -> const T& {
+    [[nodiscard]] auto operator[](const size_t index) const -> const T& {
         return this->data()[index];
     }
 
     // Const accesses to elements with bound checking and allows for negative indexing
     [[nodiscard]] auto at(const int index) const -> const T& {
-        assert(index < static_cast<int>(this->size()));
         if (index < 0 && index + static_cast<int>(this->size()) >= 0) {
             return this->data()[this->size() + index];
+        }
+        if (index >= static_cast<int>(this->size()) || index + static_cast<int>(this->size()) < 0 )) {
+            throw std::out_of_range(std::format(
+                "Index {}, is out of range for DenseVector of size {}", index, this->size()
+                ));
         }
         return this->data()[index];
     }
@@ -52,31 +57,321 @@ public:
         return this->data()[index];
     }
 
-    // Self scalar additions
-    void operator+=(const T scalar) {
-        for (size_t i = 0; i < this->size(); ++i) {
+    //-----------------------------
+    // Scalar operations
+    // ----------------------------
+
+    /**
+     * @brief In-place scalar addition
+     * @param scalar value to add to each element
+     * @return *this
+     */
+    auto operator+=(const T scalar) noexcept -> DenseVector& {
+        for (size_t i{0}; i < this->size(); ++i) {
             this->data()[i] += scalar;
         }
-    }
-    // Self scalar subtraction
-    void operator-=(const T scalar) {
-        for (size_t i = 0; i < this->size(); ++i) {
-            this->data()[i] -= scalar;
-        }
-    }
-    // Self scalar multiplication
-    void operator*=(const T scalar) {
-        for (size_t i = 0; i < this->size(); ++i) {
-            this->data()[i] *= scalar;
-        }
+        return *this;
     }
 
-    // Self scalar division
-    void operator/=(const T scalar) {
-        for (size_t i = 0; i < this->size(); ++i) {
+    /**
+     * @brief In-place scalar subtraction
+     * @param scalar value to subtract from each element
+     * @return *this
+     */
+    auto operator-=(const T scalar) noexcept -> DenseVector& {
+        for (size_t i{0}; i < this->size(); ++i) {
+            this->data()[i] -= scalar;
+        }
+        return *this;
+    }
+
+    /**
+     * @brief In-place scalar multiplication
+     * @param scalar value to multiply each element by
+     * @return *this
+     */
+    auto operator*=(const T scalar) noexcept -> DenseVector& {
+        for (size_t i{0}; i < this->size(); ++i) {
+            this->data()[i] *= scalar;
+        }
+        return *this;
+    }
+
+    /**
+     * @brief In-place scalar division
+     * @param scalar value to divide each element by
+     * @return *this
+     */
+    auto operator/=(const T scalar) noexcept -> DenseVector& {
+        for (size_t i{0}; i < this->size(); ++i) {
             assert(this->data()[i] != 0);
             this->data()[i] /= scalar;
         }
+        return *this;
+    }
+
+    /**
+     * @brief Scalar addition operation
+     * @param scalar value to add to each element
+     * @return DenseVector with each element added by scalar
+     */
+    [[nodiscard]] auto operator+(const T scalar) -> DenseVector {
+        DenseVector result(*this);
+        result += scalar;
+        return result;
+    }
+
+    /**
+     * @brief Scalar subtraction operation
+     * @param scalar value to subtract from each element
+     * @return DenseVector with each element subtracted by scalar
+     */
+    [[nodiscard]] auto operator-(const T scalar) -> DenseVector {
+        DenseVector result(*this);
+        result -= scalar;
+        return result;
+    }
+
+    /**
+     * @brief Scalar multiplication operation
+     * @param scalar value to multiply each element by
+     * @return DenseVector with each element multiplied by scalar
+     */
+    [[nodiscard]] auto operator*(const T scalar) -> DenseVector {
+        DenseVector result(*this);
+        result *= scalar;
+        return result;
+    }
+
+    /**
+     * @brief Scalar division operation
+     * @param scalar value to divide each element by
+     * @return DenseVector with each element divided by scalar
+     */
+    [[nodiscard]] auto operator/(const T scalar) -> DenseVector {
+        DenseVector result(*this);
+        result /= scalar;
+        return result;
+    }
+
+    //-------------------------------------------------
+    // DenseVector - Container arithmetic operations
+    //-------------------------------------------------
+
+    /**
+     * @brief In-place vector addition with any container of equal size
+     * @param Vector to add into DenseVector (element by element)
+     * @return *this
+     */
+     auto operator+=(std::span<const T> other) -> DenseVector& {
+        assert(this->size() == other.size() && "Size mismatch for vector-vector summation");
+
+        for (size_t i{0}; i < this->size(); ++i) {
+          this->data()[i] += other[i];
+        }
+
+        return *this;
+    }
+
+    /**
+    * @brief In-place vector subtraction with any container of equal size
+    * @param Vector to subtract from DenseVector (element by element)
+    * @return *this
+    */
+    auto operator-=(std::span<const T> other) -> DenseVector& {
+       assert(this->size() == other.size() && "Size mismatch for vector-vector summation");
+
+        for (size_t i{0}; i < this->size(); ++i) {
+            this->data()[i] -= other[i];
+        }
+
+        return *this;
+    }
+
+    /**
+    * @brief In-place vector multiplication with any container of equal size
+    * @param Vector to multiply with DenseVector (element by element)
+    * @return *this
+    */
+    auto operator*=(std::span<const T> other) -> DenseVector& {
+        if (this->size() != other.size()) {
+            throw std::runtime_error(std::format(
+            "Size mismatch for vector-vector operation: lhs size = {}, rhs size = {}",
+            this->size(),
+            other.size()
+            ));
+        }
+
+        for (size_t i{0}; i < this->size(); ++i) {
+            this->data()[i] *= other[i];
+        }
+
+        return *this;
+    }
+
+    /**
+    * @brief In-place vector division with any container of equal size
+    * @param Vector to divide from DenseVector (element by element)
+    * @return *this
+    */
+    auto operator/=(std::span<const T> other) -> DenseVector& {
+       assert(this->size() == other.size() && "Size mismatch for vector-vector summation");
+
+        for (size_t i{0}; i < this->size(); ++i) {
+            this->data()[i] /= other[i];
+        }
+        return *this;
+    }
+
+    // DenseVector - container additions (element
+    [[nodiscard]] auto operator+(std::span<const T> other) const -> DenseVector {
+        DenseVector result(*this);
+        result += other;
+        return result;
+    }
+
+    // DenseVector - container subtraction
+    [[nodiscard]] auto operator-(std::span<const T> other) const -> DenseVector {
+        DenseVector result(*this);
+        result -= other;
+        return result;
+    }
+
+    // DenseVector - container multiplication
+    [[nodiscard]] auto operator*(std::span<const T> other) const -> DenseVector {
+        DenseVector result(*this);
+        result += other;
+        return result;
+    }
+
+    [[nodiscard]] auto operator/(std::span<const T> other) const -> DenseVector {
+        DenseVector result(*this);
+        result += other;
+        return result;
+    }
+
+    [[nodiscard]] auto dot(std::span<const T> other) const -> T {
+        assert(this->size() == other.size() && "Size mismatch for vector-vector summation");
+
+        T sum{};
+        for (size_t i{0}; i < this->size(); ++i) {
+            sum += this->data()[i] * other[i];
+        }
+        return sum;
+    }
+
+    //-------------------------------------------------
+    // DenseVector - DenseVector arithmetic operations
+    //-------------------------------------------------
+
+    /**
+     * @brief In-place vector addition with any container of equal size
+     * @param Vector to add into DenseVector (element by element)
+     * @return *this
+     */
+     auto operator+=(const DenseVector other) -> DenseVector& {
+        assert(this->size() == other.size() && "Size mismatch for vector-vector summation");
+
+        for (size_t i{0}; i < this->size(); ++i) {
+          this->data()[i] += other[i];
+        }
+
+        return *this;
+    }
+
+    /**
+    * @brief In-place vector subtraction with any container of equal size
+    * @param Vector to subtract from DenseVector (element by element)
+    * @return *this
+    */
+    auto operator-=(const DenseVector other) -> DenseVector& {
+       assert(this->size() == other.size() && "Size mismatch for vector-vector summation");
+
+        for (size_t i{0}; i < this->size(); ++i) {
+            this->data()[i] -= other[i];
+        }
+
+        return *this;
+    }
+
+    /**
+    * @brief In-place vector multiplication with any container of equal size
+    * @param Vector to multiply with DenseVector (element by element)
+    * @return *this
+    */
+    auto operator*=(const DenseVector other) -> DenseVector& {
+        if (this->size() != other.size()) {
+            throw std::runtime_error(std::format(
+            "Size mismatch for vector-vector operation: lhs size = {}, rhs size = {}",
+            this->size(),
+            other.size()
+            ));
+        }
+
+        for (size_t i{0}; i < this->size(); ++i) {
+            this->data()[i] *= other[i];
+        }
+
+        return *this;
+    }
+
+    /**
+    * @brief In-place vector division with any container of equal size
+    * @param Vector to divide from DenseVector (element by element)
+    * @return *this
+    */
+    auto operator/=(const DenseVector other) -> DenseVector& {
+       assert(this->size() == other.size() && "Size mismatch for vector-vector summation");
+
+        for (size_t i{0}; i < this->size(); ++i) {
+            this->data()[i] /= other[i];
+        }
+        return *this;
+    }
+
+    // DenseVector - container additions (element
+    [[nodiscard]] auto operator+(const DenseVector other) const -> DenseVector {
+        DenseVector result(*this);
+        result += other;
+        return result;
+    }
+
+    // DenseVector - container subtraction
+    [[nodiscard]] auto operator-(const DenseVector other) const -> DenseVector {
+        DenseVector result(*this);
+        result -= other;
+        return result;
+    }
+
+    // DenseVector - container multiplication
+    [[nodiscard]] auto operator*(const DenseVector other) const -> DenseVector {
+        DenseVector result(*this);
+        result += other;
+        return result;
+    }
+
+    [[nodiscard]] auto operator/(const DenseVector other) const -> DenseVector {
+        DenseVector result(*this);
+        result += other;
+        return result;
+    }
+
+    [[nodiscard]] auto dot(const DenseVector other) const -> T {
+        assert(this->size() == other.size() && "Size mismatch for vector-vector summation");
+
+        T sum{};
+        for (size_t i{0}; i < this->size(); ++i) {
+            sum += this->data()[i] * other[i];
+        }
+        return sum;
+    }
+
+    [[nodiscard]] auto norm2() const noexcept -> T {
+        T sqrSum{};
+        for (int i{0}; i < this->size(); ++i) {
+            sqrSum += this->data()[i] * this->data()[i];
+        }
+        return std::sqrt(sqrSum);
     }
 };
 
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,16 +1,26 @@
 #include <gtest/gtest.h>
 #include <tensile.hpp>
+#include <vector>
 
 class DenseVectorTest : public testing::Test {
 protected:
 
     DenseVector<double> vec{1.0, 2.0, 3.0, 4.0, 5.0};
+    std::vector<double> vec2{1.0, 2.0, 3.0, 4.0, 5.0};
+    DenseVector<double> vec3{1.0, 2.0, 3.0, 4.0, 5.0};
     DenseVector<double> empty_vec;
+
 };
+
+TEST_F(DenseVectorTest, InitializationAfterInstantiationWorks) {
+    empty_vec = {1.0, 2.0, 3.0, 4.0, 5.0};
+    ASSERT_EQ(empty_vec.size(), vec.size());
+    ASSERT_EQ(empty_vec[0], vec[0]);
+}
 
 TEST_F(DenseVectorTest, VectorInitializationWorks) {
     ASSERT_EQ(vec.size(), 5) << "vec.size() does not equal 5, instead is " << vec.size() << '\n' ;
-    for (size_t i = 0; i < vec.size(); ++i) {
+    for (size_t i{0}; i < vec.size(); ++i) {
         ASSERT_EQ(vec[i], static_cast<double>(i + 1));
     }
 }
@@ -27,30 +37,84 @@ TEST_F(DenseVectorTest, ElementRetrievalandAssignmentBoundsChecking) {
     ASSERT_EQ(vec.at(1), 1.5);
     ASSERT_EQ(vec.at(-1), 5.0);
 }
-TEST_F(DenseVectorTest, SelfAdditionWorks) {
+
+TEST_F(DenseVectorTest, ScalarAdditionWorks) {
     vec += 1.0;
-    for (size_t i = 0; i < vec.size(); ++i) {
+    for (size_t i{0}; i < vec.size(); ++i) {
         ASSERT_EQ(vec[i], i + 2.0);
     }
-}
-
-TEST_F(DenseVectorTest, SelfSubtractionWorks) {
-    vec -= 1.0;
-    for (size_t i = 0; i < vec.size(); ++i) {
-        ASSERT_EQ(vec[i], static_cast<double>(i));
+    DenseVector<double> result = vec + 1.0;
+    for (size_t i{0}; i < result.size(); ++i) {
+        ASSERT_EQ(result[i], i + 3.0);
     }
 }
 
-TEST_F(DenseVectorTest, SelfMultiplicationWorks) {
+TEST_F(DenseVectorTest, ScalarSubtractionWorks) {
+    vec -= 1.0;
+    for (size_t i{0}; i < vec.size(); ++i) {
+        ASSERT_EQ(vec[i], static_cast<double>(i));
+    }
+    DenseVector<double> result = vec - 1.0;
+    for (size_t i{0}; i < vec.size(); ++i) {
+        ASSERT_EQ(result[i], static_cast<double>(i) - 1.0);
+    }
+}
+
+TEST_F(DenseVectorTest, ScalarMultiplicationWorks) {
     vec *= 1.0;
-    for (size_t i = 0; i < vec.size(); ++i) {
+    for (size_t i{0}; i < vec.size(); ++i) {
+        ASSERT_EQ(vec[i], static_cast<double>(i + 1));
+    }
+    DenseVector<double> result = vec * 1.0;
+    for (size_t i{0}; i < vec.size(); ++i) {
         ASSERT_EQ(vec[i], static_cast<double>(i + 1));
     }
 }
 
-TEST_F(DenseVectorTest, SelfDivisionWorks) {
+TEST_F(DenseVectorTest, ScalarDivisionWorks) {
     vec /= 2.0;
-    for (size_t i = 0; i < vec.size(); ++i) {
+    for (size_t i{0}; i < vec.size(); ++i) {
         ASSERT_EQ(vec[i], static_cast<double>((i + 1.0) / 2.0));
+    }
+    vec *=2.0;
+    DenseVector<double> result = vec / 2.0;
+    for (size_t i{0}; i < vec.size(); ++i) {
+        ASSERT_EQ(vec[i], static_cast<double>((i + 1.0) / 2.0));
+    }
+}
+
+TEST_F(DenseVectorTest, VectorAdditionWorks) {
+    DenseVector<double> expected {2.0, 4.0, 6.0, 8.0, 10.0};
+    DenseVector<double> result = vec + vec2;
+    DenseVector<double> result2 = vec + vec3;
+    vec += vec2;
+    for (size_t i{0}; i < vec.size(); ++i) {
+      ASSERT_EQ(result[i], expected[i]);
+        ASSERT_EQ(result2[i], expected[i]);
+        ASSERT_EQ(vec[i], expected[i]);
+    }
+}
+
+TEST_F(DenseVectorTest, VectorSubtractionWorks) {
+    DenseVector<double> expected {0.0, 0.0, 0.0, 0.0, 0.0};
+    DenseVector<double> result = vec - vec2;
+    DenseVector<double> result2 = vec - vec3;
+    vec -= vec2;
+    for (size_t i{0}; i < vec.size(); ++i) {
+        ASSERT_EQ(result[i], expected[i]);
+        ASSERT_EQ(result2[i], expected[i]);
+        ASSERT_EQ(vec[i], expected[i]);
+    }
+}
+
+TEST_F(DenseVectorTest, VectorMultiplicationWorks) {
+    DenseVector<double> expected {1.0, 4.0, 9.0, 16.0, 25.0};
+    DenseVector<double> result = vec * vec2;
+    DenseVector<double> result2 = vec * vec3;
+    vec *= vec2;
+    for (size_t i{0}; i < vec.size(); ++i) {
+        ASSERT_EQ(result[i], expected[i]);
+        ASSERT_EQ(result2[i], expected[i]);
+        ASSERT_EQ(vec[i], expected[i]);
     }
 }
